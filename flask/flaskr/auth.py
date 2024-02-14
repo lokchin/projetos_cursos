cfce47437112dbb6e8d896d15b9ab19a4626ddd2
Lokchin <48932138+lokchin@users.noreply.github.com>
2024-02-14 11:20:19 -0300

    Add files via upload

---
+++ flask/flaskr/auth.py
@@ -0,0 +1,66 @@
+import functools
+from flask import (
+    Blueprint, flash, g, redirect, render_template, request, session, url_for
+)
+from werkzeug.security import check_password_hash, generate_password_hash
+from flaskr.db import get_db
+
+bp = Blueprint('auth', __name__, url_prefix='/auth')
+
+
+
+
+@bp.route('/registro', methods=('GET', 'POST'))
+def registro():
+    if request.method == 'POST':
+        nome_usuario = request.form['nome_usuario']
+        senha = request.form['senha']
+        db = get_db()
+        error = None
+
+        if not nome_usuario:
+            error = 'É preciso um usuário.'
+        elif not senha:
+            error = 'É preciso uma senha'
+        if error is None:
+            try:
+                db.execute(
+                    "INSERT INTO usuario (nome_usuario, senha) VALUES (?, ?)",
+                    (nome_usuario, generate_password_hash(senha)),
+                )
+                db.commit()
+            except db.IntegrityError:
+                error = f"Usuário {nome_usuario} já possui registro."
+            else:
+                return redirect(url_for("auth.login"))
+        flash(error)
+    return render_template('auth/register.html')
+
+
+
+
+
+@bp.route('login', methods=('GET', 'POST'))
+def login():
+    if request.method == 'POST':
+        nome_usuario = request.form['nome_usuario']
+        senha = request.form['senha']
+        db = get_db()
+        error = None
+        usuario = db.execute('SELECT * FROM usuario WHERE nome_usuario = ?', (nome_usuario,)).fetchone()
+    
+        if usuario is None:
+            error = 'Usuário não consta'
+        elif not check_password_hash(usuario['senha'], senha):
+            error = 'Senha incorreta'
+
+        if error is None:
+            session.clear()
+            session['usuario_id'] = usuario['id']
+            return redirect(url_for('index'))
+    
+        flash(error)
+
+    return render_template('auth/login.html')
+    
+    
